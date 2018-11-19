b7776ab62a0ad2abcfd03883596dca0a86375eef
jjurman <jjurman@constantcontact.com>
2018-11-18 19:17:53 -0500

    startX and startY

--- actions/cards.js
+++ actions/cards.js
@@ -1,11 +1,15 @@
+// create a new card, basically just a starterObject
 const newCard = (startX, startY) => ({
   active: false,
-  currentX: 0,
-  currentY: 0,
+  currentX: startX,
+  currentY: startY,
   initialX: null,
   initialY: null
 })
 
+// function that takes in a set of cards, a card to update, and a
+// function to update with. this is a helper method that makes it
+// easy to update just a single card, but return the whole list
 const updateCards = (cards, updateIndex, updatedCard) => {
   return cards.map((card, cardIndex) => {
     if (cardIndex !== updateIndex) return card
@@ -14,11 +18,13 @@ const updateCards = (cards, updateIndex, updatedCard) => {
 }
 
 module.exports = {
+  // create however many cards we want using newCard function
   init: () => [
-    newCard(),
-    newCard(),
-    newCard()
+    newCard(-100, -100),
+    newCard(0, 0),
+    newCard(100, 100)
   ],
+  // initiate dragging for a single card
   dragStart: (cards, {cardIndex, x, y}) => {
     return updateCards(cards, cardIndex, card => ({
       active: true,
@@ -26,11 +32,13 @@ module.exports = {
       initialY: y - card.currentY
     }))
   },
+  // this event sets all cards to not active
   dragEnd: (cards) => {
     return cards.map(card =>
       Object.assign({}, card, {active: false})
     )
   },
+  // this event drags a single card (at cardIndex)
   drag: (cards, {cardIndex, x, y}) => {
     return updateCards(cards, cardIndex, card => ({
       currentX: x - card.initialX,
